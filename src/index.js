26184a6278e0eaa06d3d3975239e84fe6a1e179a
Chris Postma <cjrpostma@gmail.com>
2020-04-14 17:04:53 -0600

    Configure redux store

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-
+import { HashRouter } from 'react-router-dom';
+import { applyMiddleware, createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/globalStyles';
-import theme from './styles/theme';
+import rootReducer from './reducers';
 
+// styles ------------------------------
+import theme from './styles/theme';
+import GlobalStyles from './styles/globalStyles';
+
+// components ------------------------------
 import App from './containers/App/App';
 
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <React.StrictMode>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <App />
+        </ThemeProvider>
+      </React.StrictMode>
+    </HashRouter>
+  </Provider>,
   document.getElementById('root')
 );
