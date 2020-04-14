38cb589b8d66f7aff1589f6ce364974915e4dc2e
Chris Postma <cjrpostma@gmail.com>
2020-04-14 15:27:22 -0600

    Add GlobalStyle, theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/globalStyles';
+import theme from './styles/theme';
 import App from './containers/App/App';
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById('root')
