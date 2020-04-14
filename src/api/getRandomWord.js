27fada2195958e4e7a9eb19337183cb6001aea3d
Chris Postma <cjrpostma@gmail.com>
2020-04-14 17:57:49 -0600

    Add getRandomWord api call

---
+++ src/api/getRandomWord.js
@@ -0,0 +1,13 @@
+export default async () => {
+  const response = await fetch(
+    `${process.env.REACT_APP_BASE_URL}/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=${process.env.REACT_APP_WORDNIK_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch a random word.');
+  }
+
+  const randomWordData = await response.json();
+
+  return randomWordData.word;
+};
