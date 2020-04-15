be177b98e7d3edf8c29fe6526b24a640290e9068
Chris Postma <cjrpostma@gmail.com>
2020-04-15 17:37:31 -0600

    Add requestCurrentWord thunk tests

---
+++ src/thunks/requestRandomWord.test.js
@@ -0,0 +1,12 @@
+import { requestRandomWord } from './requestRandomWord';
+import { fetchRandomWordSuccess, setError, setIsLoading } from '../actions';
+
+test('it calls dispatch with setLoading(true)', () => {});
+
+test('it calls fetch with the correct arguments', () => {});
+
+test('it should dispatch setError if response not ok', () => {});
+
+test('it should dispatch setIsLoading(false) if response ok', () => {});
+
+test('it should dispatch fetchRandomWordSuccess with correct argument', () => {});
