c774fcb6274060c53a407ed4fd8d15e29e8e444b
Chris Postma <cjrpostma@gmail.com>
2020-04-16 14:33:10 -0600

    Add date to DailyWord component

--- src/containers/DailyWord/DailyWord.js
+++ src/containers/DailyWord/DailyWord.js
@@ -5,11 +5,44 @@ import Header from '../../components/Header/Header';
 import StyledHeaderTitle from '../../styles/StyledHeaderTitle';
 import StyledHeaderSubtitle from '../../styles/StyledHeaderSubtitle';
 
+const d = new Date();
+
+const today = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+][d.getDay()];
+
+const day = d.getDate();
+
+const month = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+][d.getMonth()];
+
+const year = d.getFullYear();
+
 const DailyWord = () => (
   <section>
     <Header>
       <StyledHeaderTitle>The Daily Word</StyledHeaderTitle>
-      <StyledHeaderSubtitle>Today is </StyledHeaderSubtitle>
+      <StyledHeaderSubtitle>
+        Today is {`${today}, ${month} ${day}, ${year}`}
+      </StyledHeaderSubtitle>
     </Header>
   </section>
 );
