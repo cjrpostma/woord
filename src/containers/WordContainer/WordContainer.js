d11915ba08f42a110a635b0c3aba14407dc07441
Chris Postma <cjrpostma@gmail.com>
2020-04-18 13:49:11 -0600

    Add style for WordContainer

--- src/containers/WordContainer/WordContainer.js
+++ src/containers/WordContainer/WordContainer.js
@@ -1,18 +1,63 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 // components ------------------------------
 import ContentWrapper from '../../styles/ContentWrapper';
 import Header from '../../components/Header/Header';
 import StyledHeaderTitle from '../../styles/StyledHeaderTitle';
 
+const WordCardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  visibility: hidden;
+
+  && > * {
+    background: ${({ theme }) => theme.colors.magenta};
+    border-radius: ${({ theme }) => theme.styles.borderRadius};
+    color: #ffffff;
+    display: flex;
+    flex: auto;
+    flex-wrap: nowrap;
+    font-family: ${({ theme }) => theme.fontFamily.secondary};
+    font-size: 1.5em;
+    font-weight: 400;
+    justify-content: space-around;
+    margin: ${({ theme }) => theme.spacers.sm};
+    max-width: 24rem;
+    padding: 1rem 2rem;
+    text-align: center;
+    text-transform: uppercase;
+    transition: opacity 150ms linear 100ms, transform 150ms ease-in-out 100ms;
+    visibility: visible;
+  }
+
+  &&:hover > * {
+    opacity: 0.4;
+    transform: scale(0.9);
+  }
+
+  && > *:hover {
+    cursor: pointer;
+    opacity: 1;
+    transform: scale(1);
+    transition-delay: 0ms, 0ms;
+  }
+`;
+
 const WordContainer = ({ userWords }) => (
   <section>
     <Header>
       <StyledHeaderTitle>Woords</StyledHeaderTitle>
     </Header>
-    <ContentWrapper>{userWords.map(word => word.word)}</ContentWrapper>
+    <ContentWrapper>
+      <WordCardWrapper>
+        {userWords.map(word => (
+          <p>{word.word}</p>
+        ))}
+      </WordCardWrapper>
+    </ContentWrapper>
   </section>
 );
 
