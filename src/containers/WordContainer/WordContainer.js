13e6187f4765cd772c50c641ae36d15af2043417
Chris Postma <cjrpostma@gmail.com>
2020-04-18 13:12:26 -0600

    Add skeleton of WordContainer

--- src/containers/WordContainer/WordContainer.js
+++ src/containers/WordContainer/WordContainer.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const WordContainer = () => <div>WordContainer</div>;
+// components ------------------------------
+import ContentWrapper from '../../styles/ContentWrapper';
+import Header from '../../components/Header/Header';
+import StyledHeaderTitle from '../../styles/StyledHeaderTitle';
 
-export default WordContainer;
+// takes in words, displays cards
+// the cards themselves are not connected, they just open links to worddetails
+
+const WordContainer = ({ userWords }) => (
+  <section>
+    <Header>
+      <StyledHeaderTitle>Woords</StyledHeaderTitle>
+    </Header>
+    <ContentWrapper>{userWords.map(word => 'hello')}</ContentWrapper>
+  </section>
+);
+
+const mapStateToProps = state => ({
+  userWords: state.userWords,
+});
+
+export default connect(mapStateToProps)(WordContainer);
